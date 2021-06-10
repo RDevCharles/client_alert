5296f1221306b332adcdb37a0a2b93dfad7070e9
RDevCharles <rdevcharles@protonmail.com>
2021-06-10 14:59:40 -0400

    code clean up | wrap up

--- bojack.py
+++ bojack.py
@@ -1,14 +1,23 @@
 from requests_html import HTMLSession
+import smtplib
+import threading
+
 
 session = HTMLSession()
 priorV = open("bojack.txt", "r")
 v = priorV.read()
-print(v)
+
+#email instance info
+
+server = smtplib.SMTP( "smtp.gmail.com", 587 )
+server.starttls()
+server.login( '<sender email>', '<password>')
+server.sendmail( 'rondell', '<client email>', 'message' )
 
 
+#getting version number from website
 
-
-def getSiteVersion():
+def get_site_version():
     try:
         r = session.get('https://eloquent-curie-58f1a3.netlify.app')
         resRoot = r.html.find('#vNum', first=True)
@@ -20,16 +29,27 @@ def getSiteVersion():
         print('did not work')
         print(r.status_code)
 
-getSiteVersion()
+#grabbing the file that holds the current version
 
-
-def findVersion():
-    if priorV != getSiteVersion():
+def find_version():
+    print('hello')
+    if priorV != get_site_version():
         with open("bojack.txt", "w") as f:
-            f.write(getSiteVersion())
+            f.write(get_site_version())
 
-    
+#calling both function
 
-findVersion()
+def main_func():
+    get_site_version()
+    find_version()
 
-#TODO fix flow of save and compare
+def set_interval(func, sec):
+    def func_wrapper():
+        set_interval(func, sec)
+        func()
+    t = threading.Timer(sec, func_wrapper)
+    t.start()
+    return t
+
+set_interval(main_func, 5)
+
